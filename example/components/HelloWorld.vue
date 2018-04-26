3c3b8c1bd8aca0b31c6120b4241e74031def669f
limengke <752140235@qq.com>
2018-04-26 21:36:00 +0800

    先提交个基本框架

---
+++ example/components/HelloWorld.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="hello">
+    <div class="box1" v-move></div>
+    <div class="box2" v-move></div>
+    <div class="box3" v-move></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.hello{
+  position: relative;
+  width: 100%;
+  height: 100%;
+}
+.box1{
+  width:200px;
+  height:200px;
+  background-color:rgb(65,184,139);
+  position:absolute;
+}
+.box2{
+  width:200px;
+  height:200px;
+  background-color:rgb(184,65,139)
+}
+.box3{
+  width:200px;
+  height:200px;
+  background-color:rgb(184,139,65)
+}
+</style>
