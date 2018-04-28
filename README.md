07907ba8bef655b974596943d2f64895c145f831
limengke <752140235@qq.com>
2018-04-28 16:24:34 +0800

    补充readme

--- README.md
+++ README.md
@@ -5,7 +5,9 @@
 *1.0分支*在组件实例上是用单例模式,所有的用到`tooltip`组件的,均是使用的一个组件实例,大部分条件下没有问题,但是存在一个很致命的问题,
 **就是在快速切换的时候,组件会出现闪跳的现象**,所以在*2.0*分支上取消单例模式写法,一个`tooltip`组件就是单独一个组件.
 
+组件的样式参考`iview`:
 
+![](static/2.0.gif)
 
 ## 开始
 
@@ -22,12 +24,4 @@ npm run build
 # build for production and view the bundle analyzer report
 npm run build --report
 
-# run unit tests
-npm run unit
-
-# run e2e tests
-npm run e2e
-
-# run all tests
-npm test
 ```
--- example/components/HelloWorld.vue
+++ example/components/HelloWorld.vue
@@ -1,15 +1,15 @@
 <template lang="pug">
   .hello
-    .box1(v-tips="options") 测试很长很长很擦汗那个的文字
+    .box1(v-tips="options") 关关雎鸠
     br
     br
-    .box1(v-tips="options1") 测试文字2
+    .box1(v-tips="options1") 窈窕淑女，君子好逑。
     br
     br
-    .box1(v-tips="options2") 测试文字32
+    .box1(v-tips="options2") 窈窕淑女
     br
     br
-    .box1(v-tips="options3") 测试文字32
+    .box1(v-tips="options3") 求之不得
 </template>
 
 <script>
@@ -18,20 +18,20 @@ export default {
   data () {
     return {
       options: {
-        content: 'tip测试文字',
+        content: '在河之洲',
         placement: 'top-start'
       },
       options1: {
-        content: '测试文字111111111',
+        content: '参差荇菜，左右流之。',
         placement: 'right'
       },
       options2: {
-        content: '测试文字',
-        placement: 'bottom-end'
+        content: '寤寐求之',
+        placement: 'left'
       },
       options3: {
-        content: '测试文字',
-        placement: 'left'
+        content: '寤寐思服',
+        placement: 'bottom-end'
       }
     }
   }
@@ -40,6 +40,6 @@ export default {
 
 <style lang="stylus">
   .box1
-    background-color #efefef
+    //background-color #efefef
     display inline-block
 </style>
